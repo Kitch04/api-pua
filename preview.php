00580b9800b7a70158b452a63d76b802c0b11fa1
Kitch04 <55270495+Kitch04@users.noreply.github.com>
2020-03-04 17:01:40 +0800

    Update preview.php

--- preview.php
+++ preview.php
@@ -7,7 +7,7 @@ $data = json_decode($json, true);
 ?>
 
 <link rel="stylesheet" href="style.css">
-<a href="./form_create.php?id=<?php echo $id ?>"> 
+<a href="?p=add&id=<?php echo $id ?>"> 
 	<button type="button">Edit</button>
 </a> | 
 <form style="display: inline-block" method="post" action="pro_delete.php">
