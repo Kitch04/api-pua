e3b1338801a43679c466485567bc572c135b16ce
Kitch04 <55270495+Kitch04@users.noreply.github.com>
2020-03-05 16:35:07 +0800

    Update preview.php

--- preview.php
+++ preview.php
@@ -8,32 +8,35 @@ $data = json_decode($json, true);
 
 <link rel="stylesheet" href="style.css">
 
+<div class="container">
+	<table class="table">
+		<div style="float:right">
+			<a style="text-decoration: none" href="?p=add&id=<?php echo $id ?>"> 
+			<button type="button">Edit</button>
+			</a> | 
+			<form style="display: inline-block" method="post" action="pro_delete.php">
+				<input type="hidden" name="id" value="<?php echo $id ?>" />
+				<button type="submit" name="remove" value="remove">Delete</button>
+			</form>
+		</div>
 
-<table class="table">
-	<a style="text-decoration: none" href="?p=add&id=<?php echo $id ?>"> 
-		<button type="button">Edit</button>
-	</a> | 
-	<form style="display: inline-block" method="post" action="pro_delete.php">
-		<input type="hidden" name="id" value="<?php echo $id ?>" />
-		<button type="submit" name="remove" value="remove">Delete</button>
-	</form>
+		<thead class="thead-dark">
+			<tr>
+				<th>Name</th>
+				<th>Price</th>
+				<th>Category</th>
+			</tr>
+		</thead>
 	
-	<thead class="thead-dark">
-		<tr>
-			<th>Name</th>
-			<th>Price</th>
-			<th>Category</th>
-		</tr>
-	</thead>
-	
-	<tbody>
-		<tr>
-			<td><?php echo $data['name']; ?></td>
-			<td><?php echo $data['price']; ?></td>
-			<td><?php echo $data['category_name']; ?></td>
-		</tr>
-		<tr align="center">
-			<td colspan="3"><?php echo $data['description']; ?></td>
-		</tr>
-	</tbody>
-</table>
+		<tbody>
+			<tr>
+				<td><?php echo $data['name']; ?></td>
+				<td><?php echo $data['price']; ?></td>
+				<td><?php echo $data['category_name']; ?></td>
+			</tr>
+			<tr align="center">
+				<td colspan="3"><?php echo $data['description']; ?></td>
+			</tr>
+		</tbody>
+	</table>
+</div>
