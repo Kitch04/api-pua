2dd1b63181c0e54f93e3a324d8b3de480044eeaa
Kitch04 <55270495+Kitch04@users.noreply.github.com>
2020-03-05 16:39:01 +0800

    Update preview.php

--- preview.php
+++ preview.php
@@ -9,7 +9,7 @@ $data = json_decode($json, true);
 <link rel="stylesheet" href="style.css">
 
 <div class="container">
-	<table class="table">
+	<table class="table mt-5">
 		<thead class="thead-dark">
 			<tr>
 				<th>Name</th>
@@ -24,7 +24,7 @@ $data = json_decode($json, true);
 				<td><?php echo $data['name']; ?></td>
 				<td><?php echo $data['price']; ?></td>
 				<td><?php echo $data['category_name']; ?></td>
-				<td>
+				<td align="right">
 					<a style="text-decoration: none" href="?p=add&id=<?php echo $id ?>"> 
 					<button class="btn btn-primary" type="button">Edit</button>
 					</a> | 
@@ -35,7 +35,7 @@ $data = json_decode($json, true);
 				</td>
 			</tr>
 			<tr align="center">
-				<td colspan="3"><?php echo $data['description']; ?></td>
+				<td colspan="4"><?php echo $data['description']; ?></td>
 			</tr>
 		</tbody>
 	</table>
