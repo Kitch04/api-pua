b48b65a0244173ef192b3e206b6c4dd9d73e2b8b
Kitch04 <55270495+Kitch04@users.noreply.github.com>
2020-03-05 16:24:38 +0800

    Update preview.php

--- preview.php
+++ preview.php
@@ -7,15 +7,33 @@ $data = json_decode($json, true);
 ?>
 
 <link rel="stylesheet" href="style.css">
-<a href="?p=add&id=<?php echo $id ?>"> 
-	<button type="button">Edit</button>
-</a> | 
-<form style="display: inline-block" method="post" action="pro_delete.php">
-	<input type="hidden" name="id" value="<?php echo $id ?>" />
-	<button type="submit" name="remove" value="remove">Delete</button>
-</form>
 
-<h1><?php echo $data['name']; ?></h1>
-<h3><?php echo $data['price']; ?></h3>
-<h3><?php echo $data['description']; ?></h3>
-<h3><?php echo $data['category_name']; ?></h3>
+
+<table class="table table-bordered">
+	<a href="?p=add&id=<?php echo $id ?>"> 
+		<button type="button">Edit</button>
+	</a> | 
+	<form style="display: inline-block" method="post" action="pro_delete.php">
+		<input type="hidden" name="id" value="<?php echo $id ?>" />
+		<button type="submit" name="remove" value="remove">Delete</button>
+	</form>
+	
+	<thead class="thead-dark">
+		<tr>
+			<th>Name</th>
+			<th>Price</th>
+			<th>Category</th>
+		</tr>
+	</thead>
+	
+	<tbody>
+		<tr>
+			<td><?php echo $data['name']; ?></td>
+			<td><?php echo $data['price']; ?></td>
+			<td><?php echo $data['category_name']; ?></td>
+		</tr>
+		<tr>
+			<td><?php echo $data['description']; ?></td>
+		</tr>
+	</tbody>
+</table>
