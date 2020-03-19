4bd2c4f664264d2a21cef89d5bed7ab42dffbd71
Kitch04 <55270495+Kitch04@users.noreply.github.com>
2020-03-19 17:50:14 +0800

    Update table.php

--- table.php
+++ table.php
@@ -11,7 +11,15 @@ $list = $data['records'];
 ?>
 
 <link rel="stylesheet" href="style.css">
-
+<a href="index.php" onclick="signOut();">Sign out</a>
+<script>
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+  }
+</script>
 <table class="table table-hover" align="center">
 
 		<thead class="thead-dark">
